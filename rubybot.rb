990f7f57c1a8d399a9a9ada6453eb9ed785ec57e
Kagemaru <new.mysteryman@gmail.com>
2008-06-01 20:45:16 +0200

    Fixed debugoutput | changed !say && !do | added quotes.txtDebugoutput propperly goes to a new line now and the last line doesn't wait for ^C to output anymore. !say and !do should now be able to handle !say #idp <stuff> syntax, but this is untested yet.

--- rubybot.rb
+++ rubybot.rb
@@ -1,47 +1,67 @@
 require 'IRC'
 
-SERVER	= "irc.rizon.net"
+#Configurations for the bot.
+#ToDo: Move to a seperate file.
+#ToDo: Make it less static (multiple channels, multiple nicks... maybe even multiple servers?)
+
+SERVER	= "idp.ath.cx"
 PORT	= 6667
 CHANNEL	= "#idp"
-NICK	= "rubytestbot"
+NICK	= "rubybot"
 PASS	= "mysticfire"
 NAME	= "no one really"
 
+#End of configuration
+
+#Bot initialization
 $bot = IRC.new(NICK, SERVER, PORT, NAME)
 
+#End of initialization
+
+#methods
 def msg(target, message)
 	$bot.send_message(target, message)
 end
 def act(target, action)
 	$bot.send_action(target, action)
 end
+#End of methods
+
+#Event handlers. React on channel/user/serverevents
+
+##Identify for the nick and let it join some channels after the MOTD
 IRCEvent.add_callback('endofmotd') { |event|
-	msg("NickServ","identify mysticfire")
-	puts "identified!"
+	#msg("NickServ","identify mysticfire")
+	#puts "identified!"
 	$bot.add_channel(CHANNEL)
 	puts "joined " + CHANNEL + "!"
 }
-IRCEvent.add_callback('join') { |event|
-        if (event.from != NICK)
-		msg(event.channel, "Hello #{event.from}!")
-	end
-}
+
+##Greet users on join
+IRCEvent.add_callback('join') { |event| msg(event.channel, "Hello #{event.from}!") unless (event.from == NICK) }
+##Reacts on channel/private messages
 IRCEvent.add_callback('privmsg') { |event|
-	print event.from + ": " + event.message
+	puts "#{event.from}: #{event.message}"
 	#if (event.message =~ /^!(\w+)(\W+w+)*/)
 	if (event.message =~ /^!\S/)
 		print "\t! matched."
+		var = event.message[1..-1].split
 		if (var[0] =~ /\./)
 			options = var[0].split('.')
 			count = 0
 			output = ""
 			options.each do |option|
 				count += 1
-				if count == 1 then next
+				if count == 1 then
+					next
+				end
 				case option
-					when 'name': output += "Name: "
-					when 'time': output += "Date: "
-					when 'info': output  = "Name:  Date: "
+					when 'name'
+						output += "Name: "
+					when 'time'
+						output += "Date: "
+					when 'info'
+						output  = "Name:  Date: "
 				end
 				puts "sending quoteinfo: #{output}"
 				msg(CHANNEL,output)
@@ -49,12 +69,29 @@ IRCEvent.add_callback('privmsg') { |event|
 		end
 		var = event.message[1..-1].split
 		case var[0]
+			when 'methodinfo':
+				puts "1: #{event.stats[0]}"
+				puts "2: #{event.stats[1]}"
+				puts "3: #{event.stats[2]}"
+				puts "4: #{event.stats[3]}"
+				puts "*: #{event.channel}"
+				puts $bot.channels
+				$bot.channels.each { |ch| puts ch.name }
 			when 'say':
-				print " saying: " + var[1..-1]
-				msg(CHANNEL,var[1..-1])
+				if var[1] =~ /^#/ 
+					$bot.channels.each { |channel|
+						if channel.name == var[1]
+							print " saying: #{var[2..-1].join(" ")} on channel: #{var[1]}\n"
+							msg(var[1],var[2..-1].to_s.join(" "))
+						end
+					}
+				else
+					print " saying: #{ var[1..-1].join(" ")}\n"
+					msg(event.channel,var[1..-1].join(" "))
+				end
 			when 'do':
-				print " doing: " + var[1..-1]
-				act(CHANNEL,var[1..-1])
+				print " doing: #{var[1..-1].join(" ")}\n"
+				act(event.channel,var[1..-1].join(" "))
 			when 'quote':
 				print " sending quote: "
 				file = File.open('quotes.txt')
@@ -62,19 +99,18 @@ IRCEvent.add_callback('privmsg') { |event|
 				count = 0
 				if (var[1].to_i > 0) && (var[1].to_i <= file.lineno) then number = var[1].to_i
 				else number = (rand(file.lineno) + 1)
-				end
+				end				
 				file.rewind
 				file.each_line do |line|
 					if ((count += 1) == number)
-						print line.chomp
+						print line.chomp + "\n"
 						msg(CHANNEL,line.chomp)
 					end
 				end
 				file.close
 		end
 	end
-	print "\n"
 }
+#End of event handlers
 
 $bot.connect
-
